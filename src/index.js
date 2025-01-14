7dd5f3ff87d96ced3bbb0f8ed19b5514271240d5
Brandon Bakus <brandonbakus@gmail.com>
2025-01-14 13:32:39 -0500

    YAY

--- src/index.js
+++ src/index.js
@@ -2,23 +2,85 @@
 
 // Callbacks
 const handleClick = (ramen) => {
-  // Add code
+  const detalImage = document.querySelector('.detail-image')
+  detalImage.src = ramen.image
+  const ramenName = document.querySelector('.name')
+  ramenName.textContent = ramen.name
+  const restaurant = document.querySelector('.restaurant')
+  restaurant.textContent = ramen.restaurant
+  const comment = document.querySelector('#comment-display')
+  comment.textContent = ramen.comment
+  const ratingDisplay = document.querySelector('#rating-display')
+  ratingDisplay.textContent = ramen.rating
+  
 };
 
 const addSubmitListener = () => {
-  // Add code
+  const submitForm = document.querySelector('#new-ramen')
+  
+  submitForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    const newName = document.querySelector('#new-name')
+    const newRestaurant = document.querySelector('#new-restaurant')
+    const newImage = document.querySelector('#new-image')
+    const newRating = document.querySelector('#new-rating')
+    const newComment = document.querySelector('#new-comment')
+    const name = document.querySelector('.name')
+    const restaurantName = document.querySelector('.restaurant')
+    const detailImage = document.querySelector('.detail-image')
+    const comment = document.querySelector('#comment-display')
+    const ratingDisplay = document.querySelector('#rating-display')
+    const menuDisplay = document.querySelector('#ramen-menu')
+
+    name.textContent = newName.value 
+    restaurantName.textContent = newRestaurant.value 
+    detailImage.src = newImage.value
+    comment.textContent = newComment.value 
+    ratingDisplay.textContent = newRating.value
+
+    const newThumbnail = document.createElement('img')
+    newThumbnail.src = newImage.value
+    menuDisplay.appendChild(newThumbnail)
+    newThumbnail.addEventListener('click', () => {
+      handleClick({
+        name: newName.value,
+        restaurant: newRestaurant.value,
+        image: newImage.value
+
+        
+      })
+     
+    })
+  }) 
 }
 
 const displayRamens = () => {
-  // Add code
+  fetch('http://localhost:3000/ramens')
+  .then(response => response.json())
+  .then(ramens => {
+    ramens.forEach(ramen => {
+    const menuDisplay = document.querySelector('#ramen-menu')
+    const ramenThumbnail = document.createElement('img')  
+    ramenThumbnail.src = ramen.image
+    menuDisplay.appendChild(ramenThumbnail)
+    ramenThumbnail.addEventListener('click', () => {
+      handleClick(ramen)
+    })
+    
+    })
+  })
 };
 
 const main = () => {
-  // Invoke displayRamens here
-  // Invoke addSubmitListener here
+  displayRamens()
+  addSubmitListener()
+  
 }
 
-main()
+document.addEventListener('DOMContentLoaded', () => {
+  main()
+})
 
 // Export functions for testing
 export {
